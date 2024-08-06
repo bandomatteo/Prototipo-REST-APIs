6c23d6f73f92941b55a4cd4eb72b811a557976ee
Matteo Bando <bandomatteo@gmail.com>
2024-08-06 16:48:38 +0200

    fist commit

---
+++ src/test/java/com/bandomatteo/Prototipo1/Prototipo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandomatteo.Prototipo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prototipo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
