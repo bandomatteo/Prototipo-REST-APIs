214c446217d48dc7f9afe0e6f2c90dc90159c633
Matteo Bando <bandomatteo@gmail.com>
2024-08-06 23:47:50 +0200

    feat: Update repository interfaces to extend Spring Data CrudRepository

--- src/main/java/com/bandomatteo/Prototipo1/repositories/AuthorRepository.java
+++ src/main/java/com/bandomatteo/Prototipo1/repositories/AuthorRepository.java
@@ -1,4 +1,9 @@
 package com.bandomatteo.Prototipo1.repositories;
 
-public interface AuthorRepository {
+import com.bandomatteo.Prototipo1.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/com/bandomatteo/Prototipo1/repositories/BookRepository.java
+++ src/main/java/com/bandomatteo/Prototipo1/repositories/BookRepository.java
@@ -1,4 +1,9 @@
 package com.bandomatteo.Prototipo1.repositories;
 
-public interface BookRepository {
+import com.bandomatteo.Prototipo1.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Integer> {
 }
