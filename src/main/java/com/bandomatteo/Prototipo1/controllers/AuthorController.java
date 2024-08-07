76cddd9b68f9b6b95e7ae5fb504be271a697e202
Matteo Bando <bandomatteo@gmail.com>
2024-08-07 16:08:18 +0200

    feat: Add endpoint to Read (CRUD) Authors

--- src/main/java/com/bandomatteo/Prototipo1/controllers/AuthorController.java
+++ src/main/java/com/bandomatteo/Prototipo1/controllers/AuthorController.java
@@ -6,9 +6,13 @@ import com.bandomatteo.Prototipo1.mappers.Mapper;
 import com.bandomatteo.Prototipo1.services.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -30,5 +34,16 @@ public class AuthorController {
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
 
         return new ResponseEntity<>(authorMapper.mapto(savedAuthorEntity), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path="/authors")
+    public List<AuthorDto> listAuthors(){
+        List<AuthorEntity> authors = authorService.findAll();
+
+        return authors.stream()
+                .map(authorMapper::mapto)
+                .collect(Collectors.toList());
+
+
     }
 }
--- src/main/java/com/bandomatteo/Prototipo1/services/AuthorService.java
+++ src/main/java/com/bandomatteo/Prototipo1/services/AuthorService.java
@@ -2,7 +2,11 @@ package com.bandomatteo.Prototipo1.services;
 
 import com.bandomatteo.Prototipo1.domain.entities.AuthorEntity;
 
+import java.util.List;
+
 
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+
+    List<AuthorEntity> findAll();
 }
